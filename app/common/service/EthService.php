9905daad5dc498deb305c9ea694713406090dbcd
zhangzhenzhen <130043536@qq.com>
2024-05-31 17:09:50 +0800

    x

--- app/common/service/EthService.php
+++ app/common/service/EthService.php
@@ -14,16 +14,14 @@ class EthService extends ComService
         $url = 'https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xdAC17F958D2ee523a2206206994597C13D831ec7&address='.$address.'&tag=latest&apikey=I258Q362FE5J2YQN7RQF5XES8MZVN7D8KM';
         $ret = curl_http($url);
         if(!$ret) return 0;
-        $res = json_decode($ret,true);
-        return $res['result'];
+        return $ret['result'];
     }
 
     public function getOrderList($address){
         $url = 'https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=0xdAC17F958D2ee523a2206206994597C13D831ec7&address='.$address.'&page=1&offset=50&startblock=0&endblock=27025780&sort=asc&apikey=I258Q362FE5J2YQN7RQF5XES8MZVN7D8KM';
         $ret = curl_http($url);
         if(!$ret) return 0;
-        $res = json_decode($ret,true);
-        return $res['result'];
+        return $ret['result'];
     }
 
     public function payUsdt(){
