d20d0cac79e0a9dd665cc54cfb2082e101056851
zhangzhenzhen <130043536@qq.com>
2024-05-31 17:03:28 +0800

    x

--- app/common/service/EthService.php
+++ app/common/service/EthService.php
@@ -5,12 +5,7 @@ namespace app\common\service;
 class EthService extends ComService
 {
     public function getEthMoney($address){
-        $url = 'https://api.etherscan.io/api
-       ?module=account
-       &action=balance
-       &address='.$address.'
-       &tag=latest
-       &apikey=I258Q362FE5J2YQN7RQF5XES8MZVN7D8KM';
+        $url = 'https://api.etherscan.io/api?module=account&action=balance&address='.$address.'&tag=latest&apikey=I258Q362FE5J2YQN7RQF5XES8MZVN7D8KM';
         $ret = getCurlData($url);
         if(!$ret) return 0;
         $res = json_decode($ret,true);
