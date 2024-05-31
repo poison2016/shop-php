ba0645419fab7b0e6736d27a9ae9119dcdfb48d8
zhangzhenzhen <130043536@qq.com>
2024-05-31 17:12:50 +0800

    x

--- app/common/service/EthService.php
+++ app/common/service/EthService.php
@@ -13,8 +13,9 @@ class EthService extends ComService
     public function getUsdtMoney($address){
         $url = 'https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xdAC17F958D2ee523a2206206994597C13D831ec7&address='.$address.'&tag=latest&apikey=I258Q362FE5J2YQN7RQF5XES8MZVN7D8KM';
         $ret = curl_http($url);
-        if(!$ret) return 0;
         var_dump($ret);
+        if(!$ret) return 0;
+
         return $ret['result'];
     }
 
