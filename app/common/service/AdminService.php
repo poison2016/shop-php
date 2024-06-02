6f93bee89f13de44d18ce5a4011ef200e29c4e33
黑夜 <130043536.com>
2024-06-02 19:25:17 +0800

    x

--- app/common/service/AdminService.php
+++ app/common/service/AdminService.php
@@ -86,7 +86,7 @@ class AdminService extends ComService
     }
 
     public function orderList(){
-        $data =  $this->orderModel->alias('o')->field('o.*,g.contract_name,g.img,g.yield,g.revenue_type')
+        $data =  $this->orderModel->alias('o')->field('o.*,g.contract_name,g.img,g.yield,g.revenue_type,u.user_name')
             ->join('t_goods g','g.id = o.contract_id','LEFT')
             ->join('tz_user u','u.user_id = o.user_id','LEFT')
             ->paginate(tp_page(15));
